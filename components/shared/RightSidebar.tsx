1f2209c98c04d91dc8ee41bda90948578bdb3e19
Victor Azevedo <victorazesc@hotmail.com>
2024-03-18 09:33:41 -0300

    first commit

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,12 @@
+export default function Rightsidebar() {
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="felx flex-1 felx-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+            </div>
+            <div className="felx flex-1 felx-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+            </div>
+        </section>
+    )
+}
