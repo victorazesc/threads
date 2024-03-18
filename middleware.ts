9472a1197135b1db727a841c02a26b6b4397b0fe
Victor Azevedo <victorazesc@hotmail.com>
2024-03-18 11:56:01 -0300

    fix: auth

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,7 @@
-// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
-// Copy the middleware code as it is from the above resource
-
 import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/webhook/clerk'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/api/webhook/clerk'],
-});
+ 
+export default authMiddleware();
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
