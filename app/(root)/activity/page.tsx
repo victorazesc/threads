c0633a597592b24af5f4e8ef71166f9cdad57a2e
Victor Azevedo <victorazesc@hotmail.com>
2024-05-01 13:03:42 -0300

    refactor: activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -21,37 +21,35 @@ async function Page() {
   const activity = await getActivity(userInfo._id);
 
   return (
-    <>
-      <h1 className='head-text'>Activity</h1>
-
-      <section className='mt-10 flex flex-col gap-5'>
-        {activity.length > 0 ? (
-          <>
-            {activity.map((activity) => (
-              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
-                <article className='activity-card'>
-                  <Image
-                    src={activity.author.image}
-                    alt='user_logo'
-                    width={20}
-                    height={20}
-                    className='rounded-full object-cover'
-                  />
-                  <p className='!text-small-regular text-light-1'>
-                    <span className='mr-1 text-primary-500'>
-                      {activity.author.name}
-                    </span>{" "}
-                    replied to your thread
-                  </p>
-                </article>
-              </Link>
-            ))}
-          </>
-        ) : (
-          <p className='!text-base-regular text-light-3'>No activity yet</p>
-        )}
-      </section>
-    </>
+    <section className='mt-10 flex flex-col gap-5 flex-grow h-full'>
+      {activity.length > 0 ? (
+        <>
+          {activity.map((activity) => (
+            <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+              <article className='activity-card'>
+                <Image
+                  src={activity.author.image}
+                  alt='user_logo'
+                  width={20}
+                  height={20}
+                  className='rounded-full object-cover'
+                />
+                <p className='!text-small-regular text-light-1'>
+                  <span className='mr-1 text-primary-500'>
+                    {activity.author.name}
+                  </span>{" "}
+                  replied to your thread
+                </p>
+              </article>
+            </Link>
+          ))}
+        </>
+      ) : (
+        <div className="flex flex-col flex-grow items-center justify-center">
+          <span className='text-secondary'>Nenhuma atividade.</span>
+        </div>
+      )}
+    </section>
   );
 }
 
